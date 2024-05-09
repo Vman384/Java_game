475fc9f70642eaa3d3e4edbe93c35e6c6705343c
WeizeY <117166336+WeizeY@users.noreply.github.com>
2024-05-10 01:31:48 +1000

    added javadocs for Mathemactics class

--- src/game/utility/Mathematics.java
+++ src/game/utility/Mathematics.java
@@ -2,7 +2,18 @@ package game.utility;
 
 import edu.monash.fit2099.engine.positions.Location;
 
+/**
+ * A utility class containing mathematical operations.
+ */
 public class Mathematics {
+
+    /**
+     * Calculates the Manhattan distance between two locations.
+     *
+     * @param a the first location
+     * @param b the second location
+     * @return the Manhattan distance between the two locations
+     */
     public static int distance(Location a, Location b) {
         return java.lang.Math.abs(a.x() - b.x()) + java.lang.Math.abs(a.y() - b.y());
     }
